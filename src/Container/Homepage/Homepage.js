a2d8401889c25a6d0720464c81397d3aa59c49a5
Seow Yong Tao <seowyongtao@Seows-MacBook-Air.local>
2019-04-29 13:46:16 +0800

    setting frontend

---
+++ src/Container/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+
+class Homepage extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default Homepage;
